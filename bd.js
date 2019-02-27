bc9ee854524e129b40742c518313c71f5235e183
Cesar Aramendi <aramendi.system@gmail.com>
2019-02-26 21:22:22 -0400

    +cambios en "server.js" a "server-curriculum.js"

--- bd.js
+++ bd.js
@@ -1,23 +1,24 @@
 const mysql = require('mysql');
 const util = require('util');
 
+
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'www.db4free.net',
-  port: 3307,
   user: 'aramendi',
+  port: 3307,
   password: '22552994',
   database: 'interoperables',
-})
-/*const pool = mysql.createPool({
-  connectionLimit: 100,
+})/*
+const pool = mysql.createPool({
+  connectionLimit: 1000,
 
-  connectTimeout  : 60 * 60 * 1000,
-  aquireTimeout   : 60 * 60 * 1000,
-  timeout         : 60 * 60 * 1000,
+  //connectTimeout  : 60 * 60 * 1000,
+  //aquireTimeout   : 60 * 60 * 1000,
+  //timeout         : 60 * 60 * 1000,
   //host: '190.170.122.7',
-  host: 'curriculum.bc.uc.edu.ve',
-  //host: 'higgs.bc.uc.edu.ve',
+  //host: 'curriculum.bc.uc.edu.ve',
+  host: 'higgs.bc.uc.edu.ve',
   //host: 'admin.higgs.bc.uc.edu.ve',
   //port: 2222,
   database: 'curriculum',
