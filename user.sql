aa15abaa20e37df7ffcc54d6533c0a7bea54b595
Cesar Aramendi <aramendi.system@gmail.com>
2019-03-17 12:48:07 -0400

    +INFORMACION DE COMANDO DE SQL

--- user.sql
+++ user.sql
@@ -1,8 +1,18 @@
 select User from mysql.user;
-
+/*
 SELECT tipo, COUNT(tipo) AS cant
 FROM carreras
 GROUP BY tipo;
+*/
+
+-- ver bases dew datos
+SHOW DATABASES;
+
+-- usar base de dato
+use curriculum 
+
+-- ver tablas
+SWOW TABLES;
 
 INSERT INTO usuarios VALUES('ad@gm.com',SHA('12'),1,NULL);
 
